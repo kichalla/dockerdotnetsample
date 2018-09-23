442ad7d264cfd2fd3236e1aad6bd3f0ae99cda10
Kiran Challa <kichalla@users.noreply.github.com>
2018-09-23 03:42:51 -0700

    Inspecting image commands

--- DockerClientLibrarySampleApp/Program.cs
+++ DockerClientLibrarySampleApp/Program.cs
@@ -1,12 +1,38 @@
 ﻿using System;
+using System.Threading.Tasks;
+using Docker.DotNet;
 
 namespace DockerClientLibrarySampleApp
 {
     class Program
     {
-        static void Main(string[] args)
+        static DockerClient client = null;
+
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            if (args.Length != 1)
+            {
+                Console.WriteLine("Provide image name to inspect");
+                return;
+            }
+
+            client = new DockerClientConfiguration(new Uri("npipe://./pipe/docker_engine"))
+                .CreateClient();
+
+            await InspectImageAsync(args[0]);
+        }
+
+        static async Task InspectImageAsync(string imageName)
+        {
+            var imageInspectResponse = await client.Images.InspectImageAsync(imageName);
+
+            Console.WriteLine();
+            Console.WriteLine("Cmd: " + string.Join(" ", imageInspectResponse.ContainerConfig.Cmd));
+
+            if (!string.IsNullOrEmpty(imageInspectResponse.Parent))
+            {
+                await InspectImageAsync(imageInspectResponse.Parent);
+            }
         }
     }
 }
