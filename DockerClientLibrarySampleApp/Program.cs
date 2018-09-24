c79a9542f2b2576d219c966d66657b3514fe9b0d
Kiran Challa <kichalla@users.noreply.github.com>
2018-09-23 17:16:39 -0700

    Dispaly sizes of the layers

--- DockerClientLibrarySampleApp/Program.cs
+++ DockerClientLibrarySampleApp/Program.cs
@@ -29,13 +29,32 @@ namespace DockerClientLibrarySampleApp
             var imageInspectResponse = await client.Images.InspectImageAsync(imageName);
 
             Console.WriteLine();
-            Console.WriteLine("RepTags: " + string.Join(" ", imageInspectResponse.RepoTags));
-            Console.WriteLine("    Cmd: " + string.Join(" ", imageInspectResponse.ContainerConfig.Cmd));
+            Console.WriteLine("    RepTags: "
+                + string.Join(" ", imageInspectResponse.RepoTags ?? Array.Empty<string>()));
+            Console.WriteLine("        Cmd: "
+                + string.Join(" ", imageInspectResponse.ContainerConfig.Cmd ?? Array.Empty<string>()));
+            Console.WriteLine("Entry point: "
+                + string.Join(" ", imageInspectResponse.ContainerConfig.Entrypoint ?? Array.Empty<string>()));
+            Console.WriteLine("       Size: " + FormatBytes(imageInspectResponse.Size));
+            Console.WriteLine("VirtualSize: " + FormatBytes(imageInspectResponse.VirtualSize));
 
             if (!string.IsNullOrEmpty(imageInspectResponse.Parent))
             {
                 await InspectImageAsync(imageInspectResponse.Parent);
             }
+        }
+
+        private static string FormatBytes(long bytes)
+        {
+            var suffixes = new[] { "B", "KB", "MB", "GB", "TB" };
+            int i;
+            double dblSByte = bytes;
+            for (i = 0; i < suffixes.Length && bytes >= 1024; i++, bytes /= 1024)
+            {
+                dblSByte = bytes / 1024.0;
+            }
+
+            return String.Format("{0:0.##} {1}", dblSByte, suffixes[i]);
         }
     }
 }
